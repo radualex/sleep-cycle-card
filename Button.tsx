2a97f280b000f23b46ac0666acfa7ad50af24308
radualex <radu.stoica1994@gmail.com>
2020-08-19 16:37:29 +0200

    2 files modified

--- Button.tsx
+++ Button.tsx
@@ -3,6 +3,10 @@ import "./Button.css";
 
 export class Button extends React.Component {
   render() {
-    return <div className="button"></div>;
+    return (
+      <div className="button-wrapper">
+        <div className="button" />
+      </div>
+    );
   }
 }
