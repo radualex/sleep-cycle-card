d29b25e438c430c33afbf351c28e8d91d5042827
radualex <radu.stoica1994@gmail.com>
2020-08-19 21:11:44 +0200

    3 files modified

--- Card.tsx
+++ Card.tsx
@@ -18,10 +18,10 @@ export class Card extends React.Component<CardProps> {
           <p className="sub-title">{this.props.subTitle}</p>
 
           <div className="wrapper-sleep-cards">
-            <InfoCard />
-            <InfoCard />
-            <InfoCard />
-            <InfoCard />
+            <InfoCard status="Awake" content="15%・1hr 35min" color="#F15D7E" />
+            <InfoCard status="Rem" content="15%・1hr 35min" color="#5DA6F1" />
+            <InfoCard status="Light sleep" content="15%・1hr 35min" color="#5D68F1" />
+            <InfoCard status="Deep sleep" content="15%・1hr 35min" color="#9E5DF1" />
           </div>
         </div>
         <div className="wrapper-button">
--- InfoCard.tsx
+++ InfoCard.tsx
@@ -3,14 +3,20 @@ import "./InfoCard.css";
 
 import { Circle } from "./Circle";
 
-export class InfoCard extends React.Component {
+interface InfoCardProps {
+  status: String;
+  content: String;
+  color: String;
+}
+
+export class InfoCard extends React.Component<InfoCardProps> {
   render() {
     return (
       <div className="wrapper-infoCard">
-        <Circle color="#F15D7E" />
+        <Circle color={this.props.color} />
         <div className="infoText">
-          <p className="infoCard-title">Awake</p>
-          <p className="infoCard-content">15%・1hr 35min</p>
+          <p className="infoCard-title">{this.props.status}</p>
+          <p className="infoCard-content">{this.props.content}</p>
         </div>
       </div>
     );
